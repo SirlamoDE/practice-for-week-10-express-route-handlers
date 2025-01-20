0b38318c7ddc799c1f8019e23f59e6c817db6d0a
@SirlamoDE <ollysurfaprod@gmail.com>
2025-01-20 06:11:52 +0000

    updated-question1-practise-solved

--- server/app.js
+++ server/app.js
@@ -25,7 +25,30 @@ const express = require('express');
 const app = express();
 
 // Your code here
+ //getting the all artist
+  app.get('/artists/:artistid', (req, res,next) => {
+    res.status(200).json(getArtistByArtistId(req.params.artistid));
+    next();
+  });
 
+  //edit a specific artist by id
+  // app.put('/artists/:artistid/', (req, res, next) => {
+  //   res.status(200).json(editArtistByArtistId(req.body.artistid))
+  //   next();
+  // });
+  
+
+app.patch('/artists/:artistid', (req, res,next) => {
+  const artistId = req.params.artistid; // Get artistId from the URL parameter
+  const data = req.body; // Get the updated data from the request body
+  res.status(200).json(editArtistByArtistId(artistId, data));
+  next();
+
+  
+});
+
+  
+  
 // DO NOT MODIFY
 if (require.main === module) {
   const port = 8000;
